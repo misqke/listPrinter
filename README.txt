cb490928591e627e12a3cd6d443ed2b7028b441d
Misqke <misqke.rust@yahoo.com>
2023-10-23 16:31:10 -0400

    init

---
+++ README.txt
@@ -0,0 +1,53 @@
+---------------------------------------------------
+
+List Builder
+Create a print friendly version of a list or lists
+
+---------------------------------------------------
+
+HOW TO USE 
+
+1. Double click the 'index.html' file to open it in your default browser.
+
+2. Optional: Select if you would like to have the application sort your categories or the items
+   in the categories alphabetically. If you leave these unselected the lists will be printed in 
+   the order they are typed in the file.
+
+3. Click 'Choose File' and select the .txt file containing your list(s).
+
+4. When the print dialog opens you can print the list or choose to save it as a pdf. 
+
+Printing Notes: 
+
+  * Styling is set to work with standard 8.5" x 11" printer paper.
+
+  * Make sure Layout is set to 'Portrait'.
+
+  * Margins are already applied and can be sets to 'None' in print options.
+
+---------------------------------------------------
+
+CREATING YOUR LIST FILE
+
+
+A .txt list can be created in Notepad or Microsoft Word. Simply type out your list in a single line,
+seperating each item with a comma (,). The first item in the line should be the category of the list.
+To start a new category simply start typing on a new line. You can leave empty lines in between the 
+categories to make it simpiler to read if you'd like.
+
+List Notes:
+
+  * If creating a file in Microsoft Word, be sure to save the file as 'Plain Text (*.txt)'.
+
+  * The application will automatically capitalize the first letter of each word.
+
+  * You can NOT use commas (,) in the items of the list.
+
+  * Any commas added at the end of a list will result in a blank line for write in items.
+
+    Example: dairy, milk, eggs, butter,,,
+
+    The above example will create a category called 'Dairy' with with milk, eggs, butter, and 3 blank
+    lines at the end.
+
+---------------------------------------------------
---
+++ main.js
@@ -0,0 +1,151 @@
+window.onload = async () => {
+  const fileBtn = document.querySelector("input[type=file]");
+  const form = document.getElementById("form");
+  const sortCategoriesCheck = document.getElementById("sortCategoriesCheck");
+  const titleInput = document.getElementById("titleInput");
+  const sortWithinCategoriesCheck = document.getElementById(
+    "sortWithinCategoriesCheck"
+  );
+
+  let itemsPerCol = 36;
+  const itemsPerPage = itemsPerCol * 3;
+
+  let itemCount = 0;
+  let sectionCount = 0;
+  let currentSection;
+  let title = "GROCERY LIST";
+
+  const reader = new FileReader();
+
+  titleInput.addEventListener("input", (e) => (title = e.target.value));
+
+  fileBtn.onchange = (e) => {
+    reader.readAsText(e.target.files[0]);
+  };
+
+  const createRow = (word) => {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    const line = document.createElement("div");
+    line.classList.add("line");
+    const text = document.createElement("p");
+    text.classList.add("item");
+    text.innerText = word;
+    if (word == "" || word == " " || word == "  ") {
+      text.classList.add("blank");
+    }
+    row.append(line, text);
+    return row;
+  };
+
+  const trimAndCapList = (list) => {
+    const fixedList = list.map((li) => {
+      li = li.replace("\r", "");
+      li = li.replace("�", "'");
+      if (li === "" || li === " " || li === "  " || li === "   ") {
+        return " ";
+      } else {
+        li = li.trim();
+
+        let word = [];
+
+        li.split(" ").forEach((lif) => {
+          if (lif[0] == "(") {
+            word.push(lif[0] + lif[1].toUpperCase() + lif.slice(2));
+          } else {
+            word.push(lif[0].toUpperCase() + lif.slice(1));
+          }
+        });
+        return word.join(" ");
+      }
+    });
+    return fixedList;
+  };
+
+  const sortList = (list) => {
+    const sortedList = [
+      list[0],
+      ...list.slice(1).sort((a, b) => {
+        if (a == "" || a == " " || a == "  ") return 1;
+        else if (b == "" || b == " " || b == "  ") return -1;
+        else if (a > b) return 1;
+        else if (a < b) return -1;
+        else return 0;
+      }),
+    ];
+    return sortedList;
+  };
+
+  const addSection = () => {
+    const section = document.createElement("section");
+    sectionCount++;
+    const pageNum = document.createElement("span");
+    pageNum.classList.add("pageNum");
+    pageNum.innerText = sectionCount;
+    section.append(pageNum);
+    document.body.append(section);
+    const titleEl = document.createElement("h1");
+    titleEl.classList.add("title");
+    titleEl.innerText = title;
+    section.append(titleEl);
+    currentSection = section;
+  };
+
+  reader.onloadend = () => {
+    const stringArr = reader.result.split("\n");
+    let lists = stringArr.map((l) => l.split(","));
+    form.classList.add("hidden");
+
+    if (sortCategoriesCheck.checked) {
+      lists = lists.sort((a, b) => (a[0] > b[0] ? 1 : a[0] < b[0] ? -1 : 0));
+    }
+
+    // add first section
+    addSection();
+
+    lists.forEach((list) => {
+      // skip any lists with one item
+      if (list.length > 1) {
+        // trim and capitalize the list items
+        list = trimAndCapList(list);
+
+        // sort lists if requested
+        if (sortWithinCategoriesCheck.checked) {
+          list = sortList(list);
+        }
+
+        // loop through lists and create elements
+        list.forEach((item, i) => {
+          // create, add, and set new section if page full
+          if (itemCount > 0 && itemCount % itemsPerPage === 0) {
+            addSection();
+          }
+
+          let row;
+          if (i === 0) {
+            row = document.createElement("div");
+            row.classList.add("row");
+            const title = document.createElement("h5");
+            title.innerText = item;
+            row.append(title);
+          } else {
+            row = createRow(item);
+          }
+          currentSection.append(row);
+          itemCount++;
+        });
+
+        // add blank space at end of list if not end of page
+        if (itemCount % itemsPerPage !== 0) {
+          const blankSpace = document.createElement("div");
+          blankSpace.classList.add("row", "empty");
+          currentSection.append(blankSpace);
+          itemCount++;
+        }
+      }
+    });
+
+    // initiate print
+    print();
+  };
+};
